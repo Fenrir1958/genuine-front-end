d2f3dbf54f67401b1fe172f27b9af50acabf4a18
Victor Manuel Diaz Vigo <victoddr1969@gmail.com>
2025-03-02 13:34:26 -0500

    feat (navbar): add component and style for navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="bg-lineal-navbar flex md:w-1/6 justify-between">
+      <div className="text-white">Logo</div>
+      <div className="text-white">Menu</div>
+    </div>
+  );
+};
